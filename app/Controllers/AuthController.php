be83c686a59db9838c05f0f563f48b7d7a52b9f5
fairuzBit <afzulfigd@gmail.com>
2026-06-03 09:30:18 +0700

    feat: implement soft deletes, add product/user models, and update authentication logic with validation

--- app/Controllers/AuthController.php
+++ app/Controllers/AuthController.php
@@ -4,25 +4,38 @@ namespace App\Controllers;
 
 use App\Controllers\BaseController;
 use CodeIgniter\HTTP\ResponseInterface;
+use App\Models\UserModel;
 
 class AuthController extends BaseController
 {
+    protected $userModel;
+
     function __construct()
     {
-
+        $this->userModel = new UserModel();
         helper('form');
     }
 
     public function login()
     {
         if ($this->request->getPost()) {
+            $rules = [
+                'username' => 'required|min_length[6]',
+                'password' => 'required|min_length[7]|numeric'
+            ];
+
+            if (!$this->validate($rules)) {
+                session()->setFlashdata('failed', $this->validator->listErrors());
+                return redirect()->back()->withInput();
+            }
+
             $username = $this->request->getVar('username');
             $password = $this->request->getVar('password');
 
-            $dataUser = ['username' => 'april', 'password' => '202cb962ac59075b964b07152d234b70', 'role' => 'admin']; // passw 123
+            $dataUser = $this->userModel->where(['username' => $username])->first();
 
-            if ($username == $dataUser['username']) {
-                if (md5($password) == $dataUser['password']) {
+            if ($dataUser) {
+                if (password_verify($password, $dataUser['password'])) {
                     session()->set([
                         'username' => $dataUser['username'],
                         'role' => $dataUser['role'],
